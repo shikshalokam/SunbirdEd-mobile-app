fe966e3a11a5956a6861d1846bbfa52dd9e538cb
vishwanathbadiger <vishwanathbadiger@vishwanaths-MacBook-Air.local>
2020-12-17 11:18:39 +0530

    issue #SB-21823 feat : migrating components and services from Diksha-Projects app

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,10 +1,6 @@
-import { from } from 'rxjs';
-
-export * from './local-storage/local-storage.service';
 export * from './db/db.service';
 export * from './loader/loader.service';
 export * from './logger/logger.service';
-export * from './event/event.service';
 export * from './attachment/attachement.service';
 export * from './utils.service';
 
--- src/app/shared/pipes/camelToTitle/camel-to-title.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CamelToTitlePipe } from './camel-to-title.pipe';
-
-describe('CamelToTitlePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CamelToTitlePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
